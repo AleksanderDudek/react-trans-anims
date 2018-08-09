15844a5d9e6b84496b9e9d00ecb71f01bd1ffdec
Aleksander Dudek <aleksander.dudek@tieto.com>
2018-08-09 13:36:41 +0200

    added transitions to html and started css

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 class Fade extends Component{
+
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: this.state.show ? false : true
+        })
+    }
+
     render(){
         return(
             <div>
--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 
+import {Transition} from 'react-transition-group';
+
 class TransitionComp extends Component{
+
+
+    state = {
+        show:true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: this.state.show ? false : true
+        })
+    }
 
     render(){
         return(
             <div>
-                Transition
+                <Transition
+                // IN decides what is the begining state of transition
+                in={this.state.show}
+                // timeout says how long does the trans take
+                    timeout={{
+                        entering: 1200,
+                        enter: 300,
+                        exiting: 500,
+                        exit: 500
+                    }}
+                >
+                    { state => 
+                    <div className={`square square-${state}`}> 
+                        { `square square-${state}` }
+                    </div> 
+                    }
+                </Transition>
+    
+                <div className="showDiv"
+                     onClick={this.showDiv}>
+                    Show/Hide
+                </div>
             </div>
-
+            
         )
     }
 }
